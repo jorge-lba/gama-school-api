b347d08da3a3252f8d0a880b60b162271413bc4e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-22 00:07:17 -0300

    feat: implementd include incorrect alternatives in question entity

--- src/modules/entities/Question.ts
+++ src/modules/entities/Question.ts
@@ -1,20 +1,37 @@
 import { Entity } from "../../core/domain/Entity";
+import { Alternative } from "./Alternative";
 
 interface IQuestionProps {
   statement: string;
+  alternatives?: Alternative[];
 }
 
 class Question extends Entity<IQuestionProps> {
   private constructor(props: IQuestionProps, id?: string) {
     super(props, id);
+    this.props.alternatives = [];
   }
 
   get statement(): string {
     return this.props.statement;
   }
 
+  get alternatives(): Alternative[] {
+    return this.props.alternatives || [];
+  }
+
   static create(props: IQuestionProps, id?: string): Question {
     return new Question(props, id);
+  }
+
+  includeIncorrectAlternatives(alternatives: string[]): void {
+    const incorrectAlternatives = alternatives.map((alternative) =>
+      Alternative.create({ text: alternative })
+    );
+
+    this.props.alternatives = this.props.alternatives?.concat(
+      incorrectAlternatives
+    );
   }
 }
 
