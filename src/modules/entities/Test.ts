470741bd91643903aeb91389f15410a08eb60cae
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-22 16:07:11 -0300

    feat: implemented add question in Test Entity

--- src/modules/entities/Test.ts
+++ src/modules/entities/Test.ts
@@ -1,20 +1,44 @@
 import { Entity } from "../../core/domain/Entity";
+import { Question } from "./Question";
 
 interface ITestProps {
   title: string;
+  questions?: Question[];
+}
+
+interface IAddQuestionProps {
+  statement: string;
+  correctAlternatives: string[];
+  incorrectAlternatives: string[];
 }
 
 class Test extends Entity<ITestProps> {
   private constructor(props: ITestProps, id?: string) {
     super(props, id);
+    this.props.questions = [];
   }
 
   get title(): string {
     return this.props.title;
   }
 
+  get questions(): Question[] {
+    return this.props.questions || [];
+  }
+
   static create(props: ITestProps, id?: string): Test {
     return new Test(props, id);
+  }
+
+  addQuestion(question: IAddQuestionProps): void {
+    const newQuestion = Question.create({
+      statement: question.statement,
+    });
+
+    newQuestion.includeCorrectAlternatives(question.correctAlternatives);
+    newQuestion.includeIncorrectAlternatives(question.incorrectAlternatives);
+
+    this.props.questions?.push(newQuestion);
   }
 }
 
