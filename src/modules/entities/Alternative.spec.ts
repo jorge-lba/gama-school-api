28a4bc0e6514d6ea21331e72b3f8e359d381a261
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-21 23:51:37 -0300

    test: should be create a new alternative and contain a text

--- src/modules/entities/Alternative.spec.ts
+++ src/modules/entities/Alternative.spec.ts
@@ -6,4 +6,10 @@ describe("Entity - Alternative", () => {
 
     expect(alternative.id).toEqual(expect.any(String));
   });
+
+  it("should be create a new alternative and contain a text", () => {
+    const alternative = Alternative.create({ text: "Alternative" });
+
+    expect(alternative.text).toEqual("Alternative");
+  });
 });
