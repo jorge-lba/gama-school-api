1a3c0c94d8c11a40d25ea5260c286c3d84ea2537
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-22 00:06:37 -0300

    test: should add alternatives in a question

--- src/modules/entities/Question.spec.ts
+++ src/modules/entities/Question.spec.ts
@@ -12,4 +12,16 @@ describe("Entity - Question", () => {
 
     expect(question.statement).toEqual("Question");
   });
+
+  it("should add alternatives in a question", () => {
+    const question = Question.create({ statement: "Question 2" });
+
+    question.includeIncorrectAlternatives(["Alternative 1", "Alternative 2"]);
+
+    const [firstAlternative, secondAlternative] = question.alternatives;
+    expect(question.alternatives.length).toEqual(2);
+    expect(secondAlternative.equals(firstAlternative)).toBeFalsy();
+    expect(firstAlternative.text).toEqual("Alternative 1");
+    expect(secondAlternative.text).toEqual("Alternative 2");
+  });
 });
