ea821c1511df8db6803c44a70fd02d50c92619d4
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-22 16:06:38 -0300

    test: should be add a question in test

--- src/modules/entities/Test.spec.ts
+++ src/modules/entities/Test.spec.ts
@@ -12,4 +12,19 @@ describe("Entity - Test", () => {
 
     expect(test.title).toEqual("test");
   });
+
+  it("should be add a question in test", () => {
+    const test = Test.create({ title: "test" });
+
+    test.addQuestion({
+      statement: "Question",
+      correctAlternatives: ["Alternative Correct"],
+      incorrectAlternatives: ["Alternative 3", "Alternative 4"],
+    });
+
+    const [question] = test?.questions;
+    expect(test.questions?.length).toEqual(1);
+    expect(question.statement).toEqual("Question");
+    expect(question.alternatives?.length).toEqual(3);
+  });
 });
