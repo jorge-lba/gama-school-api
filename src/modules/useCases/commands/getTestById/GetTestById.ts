9d8f37c3e9c44871d5bd0e81857d8b74cba133e7
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-24 22:07:25 -0300

    feat: implemented get test by id use case

---
+++ src/modules/useCases/commands/getTestById/GetTestById.ts
@@ -0,0 +1,13 @@
+import { UseCase } from "../../../../core/domain/UseCase";
+import { Test } from "../../../entities/Test";
+import { TestRepository } from "../../../repositories/TestRepository";
+
+class GetTestById implements UseCase<string> {
+  constructor(private testRepository: TestRepository) {}
+
+  async execute(id: string): Promise<Test> {
+    return this.testRepository.getTestById(id);
+  }
+}
+
+export { GetTestById };
