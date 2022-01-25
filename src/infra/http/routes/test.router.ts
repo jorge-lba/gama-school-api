2340ea5ab8c3f02a7143af58da482fba9d3e123b
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-01-24 22:49:59 -0300

    feat: implemented route to get test by id

--- src/infra/http/routes/test.router.ts
+++ src/infra/http/routes/test.router.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import { adaptRoute } from "../../../core/infra/adapters/ExpressRoutAdapter";
 import { makeCreateTestController } from "../factories/CreateTestControllerFactory";
+import { makeGetTestByIdController } from "../factories/GetTestByIdControllerFactory";
 
 const testRoutes = Router();
 
 testRoutes.post("/", adaptRoute(makeCreateTestController()));
+testRoutes.get("/:testId", adaptRoute(makeGetTestByIdController()));
 
 export { testRoutes };
