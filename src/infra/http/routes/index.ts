a87d764072398c8b14b21480dd7679d8454ce632
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-02-04 23:28:30 -0300

    feat: include account routes

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import { accountRoutes } from "./account.router";
 import { testRoutes } from "./test.router";
 
 const router = Router();
 
 router.use("/test", testRoutes);
+router.use("/account", accountRoutes);
 
 export { router };
