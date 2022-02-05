43108b2e30bdd7378c7636fe2535d704e4c815f2
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-02-04 23:24:48 -0300

    feat: implemented mapper Account

---
+++ src/modules/account/mappers/AccountMap.ts
@@ -0,0 +1,34 @@
+import { AccountPersistenceDTO } from "../DTOs/AccountPersistenceDTO";
+import { Account } from "../entities/Account";
+
+class AccountMap {
+  static toDomain({
+    name,
+    email,
+    password,
+    id,
+  }: AccountPersistenceDTO): Account {
+    return Account.create(
+      {
+        name,
+        email,
+        password,
+      },
+      id
+    );
+  }
+
+  static toPersistence(entity: Account): AccountPersistenceDTO {
+    const values = entity.values;
+
+    return {
+      id: entity.id,
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      verified: values.verified,
+    };
+  }
+}
+
+export { AccountMap };
