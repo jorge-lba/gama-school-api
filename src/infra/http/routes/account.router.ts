151ce038a21e86bea15fa6e6ee0116d979f41e0e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-02-05 22:32:12 -0300

    feat: include route to authenticate

--- src/infra/http/routes/account.router.ts
+++ src/infra/http/routes/account.router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
 import { adaptRoute } from "../../../core/infra/adapters/ExpressRoutAdapter";
+import { makeAuthenticateAccountController } from "../factories/AuthenticateAccountControllerFactory";
 import { makeRegisterAccountController } from "../factories/RegisterAccountControllerFactory";
 
 const accountRoutes = Router();
 
 accountRoutes.post("/", adaptRoute(makeRegisterAccountController()));
+accountRoutes.post(
+  "/authenticate",
+  adaptRoute(makeAuthenticateAccountController())
+);
 
 export { accountRoutes };
