12ccfe9ca94eca670814f2fca0b321899de54c96
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-02-05 21:55:59 -0300

    feat: include middleware auth account in test routes

--- src/infra/http/routes/test.router.ts
+++ src/infra/http/routes/test.router.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
+import { adaptMiddleware } from "../../../core/infra/adapters/ExpressMifflewareAdapter";
 import { adaptRoute } from "../../../core/infra/adapters/ExpressRoutAdapter";
 import { makeAddTestQuestionsController } from "../factories/AddTestQuestionsController";
 import { makeCreateTestController } from "../factories/CreateTestControllerFactory";
 import { makeGetTestByIdController } from "../factories/GetTestByIdControllerFactory";
+import { makeAuthAccountMiddleware } from "../factories/middlewares/AuthAccountMiddlewareFactory";
 
 const testRoutes = Router();
+
+testRoutes.use(adaptMiddleware(makeAuthAccountMiddleware()));
 
 testRoutes.post("/", adaptRoute(makeCreateTestController()));
 testRoutes.get("/:testId", adaptRoute(makeGetTestByIdController()));
