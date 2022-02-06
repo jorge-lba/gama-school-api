bd3af29aef1c4a0829305aa070b4b7234bf19ee0
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-02-05 23:53:31 -0300

    feat: add docs in account routes

--- src/infra/http/routes/account.router.ts
+++ src/infra/http/routes/account.router.ts
@@ -6,7 +6,86 @@ import { makeRegisterAccountController } from "../factories/RegisterAccountContr
 
 const accountRoutes = Router();
 
+/**
+ * @openapi
+ * /:
+ * /account:
+ *   post:
+ *     tags:
+ *      - Account
+ *     description: Regista um novo usuário
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do usuário.
+ *                 example: Leanne Graham
+ *               email:
+ *                type: string
+ *                description: Email do usuário.
+ *                example: leanne.graham@emial.com
+ *               password:
+ *                type: string
+ *     responses:
+ *       201:
+ *         description: O usuário foi criado com sucesso.
+ *         content:
+ *            application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *               message:
+ *                type: string
+ *                example: "Account created"
+ *               id:
+ *                type: string
+ *               token:
+ *                type: string
+ */
 accountRoutes.post("/", adaptRoute(makeRegisterAccountController()));
+
+/**
+ * @openapi
+ * /:
+ * /account/authenticate:
+ *   post:
+ *     tags:
+ *      - Account
+ *     description: Faz a autenticação de um usuário existente
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                type: string
+ *                description: Email do usuário.
+ *                example: leanne.graham@emial.com
+ *               password:
+ *                type: string
+ *     responses:
+ *       200:
+ *         description: O usuário foi autenticado com sucesso.
+ *         content:
+ *            application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *               message:
+ *                type: string
+ *                example: "Account authenticated"
+ *               id:
+ *                type: string
+ *               token:
+ *                type: string
+ */
 accountRoutes.post(
   "/authenticate",
   adaptRoute(makeAuthenticateAccountController())
